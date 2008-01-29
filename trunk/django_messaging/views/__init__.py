f96bc0b7fbb693f6d8c186d65126c71cc6cbe41f
patrainet <patrainet@05ee1ea3-b644-0410-aad4-213325eb7c31>
2008-01-29 23:31:00 +0000

    Views reorganisation

--- trunk/django_messaging/views/__init__.py
+++ trunk/django_messaging/views/__init__.py
@@ -32,30 +32,3 @@ def index(request):
   else:
     return HttpResponse('')
 
-def contacts(request):
-  if request.user.is_authenticated:
-    users=User.objects.all()
-    profile=request.user.get_profile()
-    contacts=profile.contacts.all()
-    messages=''
-    num_messages=''
-    return render_to_response('messaging/contacts.html',{'users':users,'contacts':contacts,'num_messages':num_messages,'messages':messages},context_instance=RequestContext(request))
-  else:
-    return HttpResponse('')
-
-def add_contact(request,contact_id):
-  if request.user.is_authenticated:
-    profile=request.user.get_profile()
-    contact=User.objects.get(id=contact_id)
-    profile.contacts.add(contact)
-    profile.save()
-    return contacts(request)
-  return HttpResponse('')
-
-def remove_contact(request,contact_id):
-  if request.user.is_authenticated:
-    profile=request.user.get_profile()
-    contact_user=User.objects.get(id=contact_id)
-    profile.contacts.remove(contact_user)
-    return contacts(request)
-  return HttpResponse('')
