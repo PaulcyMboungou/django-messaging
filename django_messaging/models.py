a0e957ce4d817afc0610a781eec633b8df88a82a
patrainet <patrainet@05ee1ea3-b644-0410-aad4-213325eb7c31>
2008-01-30 04:56:18 +0000

    Css classes and work on message sending

--- django_messaging/models.py
+++ django_messaging/models.py
@@ -7,17 +7,12 @@ class DmUser(models.Model):
   num_messages=models.IntegerField(default=0)
   contacts=models.ManyToManyField(User,related_name='dm_contacts')
 
-  #def is_online(self):
-
-  #def is_alive(self):
-
   def get_messages(self):
     return DfMessage.objects.filter(user=self.user)
 
   def delete_message(self,message_id):
     msg=DmMessage.objects.get(id=message_id)
     msg.delete()
-    #msg.save()
     return True
   
 class DmMessage(models.Model):
--- django_messaging/views/__init__.py
+++ django_messaging/views/__init__.py
@@ -17,19 +17,23 @@ def index(request):
   if request.user.is_authenticated():
     dm_users=DmUser.objects.all().select_related(depth=1).order_by('-last_activity')
     dm_user_current=dm_users.filter(user=request.user)[0]
+    dm_user_current_contacts=dm_user_current.contacts.all()
+    has_no_contacts=True
+    if list(dm_user_current_contacts)<>[]:
+      has_no_contacts=False
     #msg=str(dm_user_current.contacts)
     #return render_to_response('debug.html',{'message':msg},context_instance=RequestContext(request))
     user_contacts=[]
     for dm_user in dm_users:
       if dm_user.user<>request.user:
-        if dm_user.user in dm_user_current.contacts.all():
+        if dm_user.user in dm_user_current_contacts:
           dm_user.user.activity,is_offline=time_to_duration(dm_user.last_activity)
           dm_user.user.is_online=True
           if is_offline:
             dm_user.user.is_online=False
           dm_user.user.username=dm_user.user.username
-          user_contacts.append(dm_user.user)
-    return render_to_response('messaging/index.html',{'user_contacts':user_contacts,'media_url':settings.MEDIA_URL},context_instance=RequestContext(request))
+          user_contacts.append(dm_user.user) 
+    return render_to_response('messaging/index.html',{'has_no_contacts':has_no_contacts,'user_contacts':user_contacts,'media_url':settings.MEDIA_URL},context_instance=RequestContext(request))
   else:
     return HttpResponse('')
 
