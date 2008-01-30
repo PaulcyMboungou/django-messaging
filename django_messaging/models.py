8753335166afccb0aa20426a96f6d940d3838f18
patrainet <patrainet@05ee1ea3-b644-0410-aad4-213325eb7c31>
2008-01-30 08:02:20 +0000

    Sending messages

--- django_messaging/models.py
+++ django_messaging/models.py
@@ -8,14 +8,27 @@ class DmUser(models.Model):
   contacts=models.ManyToManyField(User,related_name='dm_contacts')
 
   def get_messages(self):
-    return DfMessage.objects.filter(user=self.user)
+    return DmMessage.objects.filter(user=self.user)
 
   def delete_message(self,message_id):
     msg=DmMessage.objects.get(id=message_id)
     msg.delete()
     return True
+
+  def send_message(self,to_user,message):
+    msg=DmMessage(from_user=self.user,user=to_user,message=message)
+    msg.save()
+    return True
+
+  def has_message(self):
+    msgs=self.get_messages()
+    if list(msgs)<>[]:
+      return True
+    else:
+      return False
   
 class DmMessage(models.Model):
-  user=models.ForeignKey(DmUser)
+  user=models.ForeignKey(User)
+  from_user=models.ForeignKey(User,related_name='from_user')
   date=models.DateTimeField(auto_now_add=True)
-  message=models.CharField(max_length=220)
+  message=models.CharField(max_length=255)
--- django_messaging/views/messages.py
+++ django_messaging/views/messages.py
@@ -1,18 +1,18 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response 
-from django_messaging.models import DmMessage
+from django.contrib.auth.models import User
+#from django_messaging.models import DmMessage
 from django_messaging.views import index
 
 def send_pm(request,username):
-  if request.user.is_authenticated():
-    return render_to_response('messaging/send_pm.html',{'contact_username':username})
-  else:
+  if request.user.is_anonymous():
     return HttpResponse('')
+  return render_to_response('messaging/send_pm.html',{'contact_username':username})
 
 def post_pm(request,username):
-  profile=request.user.get_profile()
-  #pm=DmMessage(user=
-  if request.user.is_authenticated():
-    return index(request)
-  else:
+  if request.user.is_anonymous():
     return HttpResponse('')
+  profile=request.user.get_profile()
+  to_user=User.objects.get(username=username)
+  profile.send_message(to_user=to_user,message=request.GET['pm'])
+  return index(request)
